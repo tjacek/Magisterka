2549450cd8be30258ed71a1489a34aa01f42f051
user <user@ubuntu1210.(none)>
2014-04-01 12:01:17 -0400

    Dodanie parametrow apriori

--- apriori/callApriori.py
+++ apriori/callApriori.py
@@ -1,14 +1,42 @@
 import os,time
 
-def experiment(datasets):
+def experiment(datasets,bounds):
     results={}
     for aprioriDataset in datasets:
- 	params=execute(0.5,0.5,1,aprioriDataset)
-        results[aprioriDataset]=params
+        for params in getParams(bounds):
+ 	    time=execute(params.minSup,params.minConf,params.workers,aprioriDataset)
+            params.time=time
+            results[aprioriDataset]=params
     return results
 
+def getParams(bounds):
+    paramsList=[]
+    for minSup in getRange(bounds.lowerSup,bounds.upperSup):
+	for minConf in getRange(bounds.lowerConf,bounds.upperConf):
+            for workers in getRange(bounds.lowerWorkers,bounds.upperWorkers,1.0):
+                param= AprioriParametrs(minSup,minConf,int(workers))
+		paramsList.append(param)
+    return paramsList
+
+def getRange(a,b,h=0.1):
+    det=b-a
+    size=int(det/h)
+    interval=[]
+    for i in range(0,size):
+	interval.append(a+i*h)
+    return interval
+
+class Bounds(object):
+    def __init__(self,lowerSup,upperSup,lowerConf,upperConf,lowerWorkers,upperWorkers):
+        self.lowerSup=lowerSup
+        self.upperSup=upperSup
+        self.lowerConf=lowerConf
+        self.upperConf=upperConf
+        self.lowerWorkers=lowerWorkers
+        self.upperWorkers=upperWorkers
+
 class AprioriParametrs(object):
-    def __init__(self,minSup,minConf,workers,time):
+    def __init__(self,minSup,minConf,workers,time=0.0):
 	self.minSup=minSup
 	self.minConf=minConf
 	self.workers=workers
@@ -34,10 +62,14 @@ def execute(sup,conf,work,data,path="/home/user/Desktop/ML/src"):
     os.system(cmd)
     time2=getTime()
     time =time2-time1
-    return AprioriParametrs(MinSup,MinConf,Workers,time)
+    return time#AprioriParametrs(MinSup,MinConf,Workers,time)
 
 def getTime():
     return int(round(time.time() * 1000))
 
 if __name__ == '__main__':
-    print(execute(0.5,0.5,1,"datasets/gen.data"))
+    datasets=["/home/user/Desktop/magisterka/apriori/datasets/gen.data",
+      "/home/user/Desktop/magisterka/apriori/datasets/mine.data"]
+    defbounds=Bounds(0.3,0.7,0.3,0.7,1,2)
+    experiment(datasets,defbounds)
+    #print(execute(0.5,0.5,1,"datasets/gen.data"))
