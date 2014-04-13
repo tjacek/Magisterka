8e64f5994638d7af9323636a226baa2fb7399e95
Jacek Treliński <tjacek@student.agh.edu.pl>
2014-04-13 22:13:09 +0200

    Podział na zbiór treningowy i testowy.

--- src/test_classifer.erl
+++ src/test_classifer.erl
@@ -6,11 +6,38 @@
 %%% @end
 %%% Created : 03. lis 2013 16:00
 %%%-------------------------------------------------------------------
--module(test).
+-module(test_classifer).
 -author("tjacek").
 
 %% API
--export([createClassifer/3,testClassifer/3 ]).
+-export([experiment/2,createClassifer/3,testClassifer/3 ]).
+
+experiment(Algorithm,Filename) ->
+  {Attributes, Instances} = mllib:read(arff,[{file,Filename}]),
+  Pred=get_rand(0.3),
+  {TestSet,TrainingSet} = split(Instances,Pred),
+  io:format("~p",[TrainingSet]).
+
+split(Instances,Rand) -> split([],[],Instances,Rand).
+split(TestSet,TrainingSet,[T|H],Rand) ->
+  R=Rand(),
+  if R -> split(TestSet,[T|TrainingSet],H,Rand);
+     true -> split([T|TestSet],TrainingSet,H,Rand)
+  end;
+split(TestSet,TrainingSet,[],Rand) ->
+  {TestSet,TrainingSet}.
+
+get_rand(P) ->
+  fun() ->
+    rand(P)
+  end.
+
+rand(P) ->
+  RanNumber=random:uniform(),
+  if
+    RanNumber>P -> false;
+    true -> true
+  end.
 
 getClassifier(Filename,Alg) ->
   {Attributes, TrainingExamples} = mllib:read(arff,[{file,Filename}]),
