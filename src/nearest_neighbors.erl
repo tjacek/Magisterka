bc6c33e0edc3bc62dd46673229b92305f9012c58
Jacek Treliński <tjacek@student.agh.edu.pl>
2014-04-16 01:03:02 +0200

    Pełna implementacja k-nearest_neighbors.

--- src/nearest_neighbors.erl
+++ src/nearest_neighbors.erl
@@ -19,20 +19,48 @@ learn(Attributes, Class, NumberedExamples, Options) ->
   LabeledPoints=lists:map(fun(X) -> get_point(X) end,NumberedExamples),
   {ok, #classifier{ algorithm = nearest_neighbors, attributes = Attributes, class = Class, specific_classifier = LabeledPoints}}.
 
-classify(_Classifier=#classifier{ algorithm = nearest_neighbors,attributes = Attributes, class = _Class, specific_classifier = W}, Example,Options) ->
-  Cat=true,
+classify(_Classifier=#classifier{ algorithm = nearest_neighbors,attributes = Attributes, class = _Class, specific_classifier = LabeledPoints}, Example,Options) ->
+  %Cat=true,
   Point=tuple_to_list(Example),
-  io:format("Example: ~p~n", [Example]),
+  Cat=nn_kernel(Point,LabeledPoints,3),
+  %io:format("Example: ~p~n", [Distances]),
   {ok, Cat}.
 
-nn_kernel(X,Points) ->
-  D=fun(P) ->
-    %utils:distance(P,X)
-    get_point(P)
+nn_kernel(X,Points,K) ->
+  Metric=fun(Instance) ->
+    Point=element(1,Instance),
+    Label=element(2,Instance),
+    D=utils:distance(Point,X),
+    {D,Label}
   end,
-  Distances=lists:map(D,Points),
-  %Sorted_distance=lists:sort(Distances),
-  io:format("OK ~p",[Distances]).
+  Compare=fun(LabeledPoint1,LabeledPoint2) ->
+    Distance1= element(1,LabeledPoint1),
+    Distance2= element(1,LabeledPoint2),
+    Distance1 < Distance2
+  end,
+  Distances=lists:map(Metric,Points),
+  Sorted_distances=lists:sort(Compare,Distances),
+  K_neighbors=element(1,lists:split(K, Sorted_distances)),
+  choose_category(K_neighbors).
+
+choose_category(K_neighbors) ->
+  {Distances,Labels}=lists:unzip(K_neighbors),
+  Categories=count_categories(Labels),
+  Comp = fun(T1,T2)->
+    Count1=element(2,T1),
+    Count2=element(2,T2),
+    Count1<Count2
+  end,
+  SortedCategories=lists:sort(Comp,Categories),
+  Cat=lists:last(SortedCategories),
+  element(1,Cat).
+
+count_categories(Labels) -> count_categories(Labels,dict:new()).
+count_categories([],Dict) -> dict:to_list(Dict);
+count_categories([H|T],Dict) ->
+  NewDict=dict:update_counter(H,1, Dict),
+  count_categories(T,NewDict).
+
 
 get_point(NumberedExamples) ->
   Example=element(2,NumberedExamples),
