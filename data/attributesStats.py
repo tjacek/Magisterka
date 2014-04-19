f69f630bb2beef92d14adb113252f9a9f0a5dc75
tjacek <user@ubuntu1210.(none)>
2014-04-19 12:29:01 -0400

    Tworzenie histogramu

--- data/attributesStats.py
+++ data/attributesStats.py
@@ -50,5 +50,64 @@ def getStr(corel):
        ws+=" "
    return str(corel)+ws
 
+class Histogram(object):
+    def __init__(self,X,Y,bins=10):
+        self.bins=bins
+        self.matrix=np.zeros(shape=(bins,bins))
+        self.getRange(X,Y)
+        self.updateAll(X,Y)
+
+    def getRange(self,X,Y):
+        self.minX=min(X)
+        self.maxX=max(X)
+        self.minY=min(Y)
+        self.maxY=max(Y)
+        self.detX=self.getDet(self.minX,self.maxX)
+        self.detY=self.getDet(self.minY,self.maxY)
+
+    def getDet(self,inf,sup):
+        return (sup-inf)/self.bins
+
+    def updateAll(self,X,Y):
+        for i in range(0,len(X)):
+            self.update(X[i],Y[i])
+
+    def update(self,x,y):
+        i,j=self.findBin(x,y)
+        self.matrix[i][j]+=1.0
+
+    def p(self,x,y): 
+        i,j=self.findBin(x,y)
+        return self.matrix[i][j]
+
+    def findBin(self,x,y):
+        for i in range(0,self.bins):
+            if(self.xRange(x,i)):
+		break
+        for j in range(0,self.bins):
+            if(self.yRange(y,j)):
+		break
+	return i,j
+ 
+    def xRange(self,x,i):
+        l=self.minX + i*self.detX
+        u=l+self.detX 
+        return (l<=x) and (x<u) 
+
+    def yRange(self,y,i):
+        l=self.minY + i*self.detY
+        u=l+self.detY 
+        return (l<=y) and (y<u)
+
+    def __str__(self):
+        return str(self.matrix)
+
+def test():
+    X=[1.0,1.0, -1.0,-1.0]
+    Y=[1.0,-1.0,1.0,-1.0]
+    H=Histogram(X,Y,2)
+    print(H)
+
 if __name__ == '__main__':
-    data=getCorelationMatrix("apriori/apriori.arff")
+    #data=getCorelationMatrix("apriori/apriori.arff")
+    test()
