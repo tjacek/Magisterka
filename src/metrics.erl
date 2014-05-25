8bac3c85a8f933f11a5fe603246c6ea1ebae9c33
Jacek Treliński <tjacek@student.agh.edu.pl>
2014-05-25 12:13:47 +0200

    Obliczenie sensitivity.

--- src/metrics.erl
+++ src/metrics.erl
@@ -11,21 +11,38 @@
 
 %% API
 -export([compute/2]).
--export([accuracy/2,error_rate/2]).
+-export([accuracy/2,error_rate/2,sensitivity/2]).
 
 compute(TrueLabels,PredLabels) ->
   Confusion_matrix=confusion_matrix(TrueLabels,PredLabels),
   Length=length(TrueLabels),
   print_cm(Confusion_matrix),
- % Metrics=[],
-  Metrics=[fun metrics:accuracy/2,fun metrics:error_rate/2],
+  Metrics=[fun metrics:accuracy/2,fun metrics:error_rate/2,fun metrics:sensitivity/2],
   Lambda=fun(F)->  F(Confusion_matrix,Length) end,
   Statistics = lists:map(Lambda,Metrics),
   io:format("~p ",[Statistics]).
 
 error_rate(Confusion_matrix,Length) ->
-  Error_rate = 1.0 - accuracy(Confusion_matrix,Length),
+  Accuracy=element(2,accuracy(Confusion_matrix,Length)),
+  Error_rate = 1.0 - Accuracy,
   {error_rate,Error_rate}.
+
+sensitivity(Confusion_matrix,Length) ->
+  Lambda=fun(Category) ->
+    TP=true_positives(Category,Confusion_matrix),
+    P=all_positives(Category,Confusion_matrix),
+    {Category,TP/P}
+  end,
+  for_all_categories(Lambda,Confusion_matrix).
+
+true_positives(Category,Confusion_matrix) ->
+   get_value(Category,Category,Confusion_matrix).
+
+all_positives(Category,Confusion_matrix) ->
+  Lambda = fun(Key) ->
+    get_value(Key,Category,Confusion_matrix)
+  end,
+  lists:sum(for_all_categories(Lambda,Confusion_matrix)).
 
 accuracy(Confusion_matrix,Length) ->
   Keys=dict:fetch_keys(Confusion_matrix),
@@ -36,7 +53,15 @@ accuracy(Confusion_matrix,Length) ->
     end,
   TP=lists:map(CorrectLabels,Keys),
   Accuracy=lists:sum(TP)/Length,
-  Accuracy.
+  {accuracy,Accuracy}.
+
+for_all_categories(Lambda,Confusion_matrix) ->
+  Categories=dict:fetch_keys(Confusion_matrix),
+  lists:map(Lambda,Categories).
+
+get_value(TrueCat,PredCat,Confusion_matrix) ->
+  Predictions=dict:fetch(TrueCat,Confusion_matrix),
+  dict:fetch(PredCat,Predictions).
 
 print_cm(Confusion_matrix)->
   CM=dict:to_list(Confusion_matrix),
