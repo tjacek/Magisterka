bf7cb9d18ae15dce56b4fd33dab64726052292ba
tjacek <user@ubuntu1210.(none)>
2014-06-08 12:12:50 -0400

    Przygotowanie zbiorow dla regresji

--- apriori/analize.py
+++ apriori/analize.py
@@ -1,6 +1,6 @@
 import sys
 sys.path.append("/home/user/Desktop/magisterka/data")
-import attributesStats as stats
+import attributesStats as stats,arff
 
 filename="apriori.arff"
 
@@ -9,6 +9,12 @@ def analizeDataset(filename):
     save(m1,filename,prefix="corelation")
     m2=stats.entropyMatrix(filename)
     save(m2,filename,prefix="entropy")
+    regression(filename)
+
+def regression(filename):
+    dataset,attr=arff.parseArff(filename,True)
+    #print(dataset)
+    train,test=arff.saveSplitedArff(dataset,"stats/",filename)
 
 def save(text,filename,prefix,path="stats/"):
     filename=filename.replace(".arff","_"+prefix+".txt")
